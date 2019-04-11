6d0d240898d455c13f4fd87e657b5c71b2b05a26
myFirstNick <Yury.Prokhorov@outlook.com>
2019-04-11 19:50:29 +0300

    Separated to multiple classes

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,13 @@ public class Main {
 
         //  Path path = Paths.get("D:\\text.txt");//Р_Мартин_Чистый_код_Создание,_анализ.pdf");
         Path path = Paths.get(UI.getAnswerWithTest("Write an existing filepath to continue",
-                x -> Paths.get(x).toFile().exists(),"File with that path doesn't exist"));
+                x -> Paths.get(x).toFile().exists(),"File with given path doesn't exist"));
 
         if (UI.getAnswerWithTest("Do you want to squeeze or restore file? (s/r)",
                 x -> x.equals("r") || x.equals("s")).equals("s")) {
 
             byte codingParameter = 1; //кол-во байт рассматриваемых в качестве 1ого символа при формировании алфавита(от 1 до 4) для дальнейших доработок сжатия
+
             Path squeezedFilePath = Paths.get(path.getParent()
                     + File.separator
                     + "squeezed_"
